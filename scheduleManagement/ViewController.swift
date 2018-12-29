e9926a9a57ae73b93f100aea0be369e147ccfdc4
Kien <kien@macbook-pro>
2018-12-29 13:02:16 +0700

    Initial Commit

---
+++ scheduleManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scheduleManagement
+//
+//  Created by Kien on 12/29/18.
+//  Copyright © 2018 Kien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
